fd271a0632080efd9e3abf8c9df85d95d06c4e8b
ernestosperanza <ernestosperanza@gmail.com>
2025-01-11 17:23:33 -0300

    feat: state machine ex

---
+++ src/main.rs
@@ -0,0 +1,104 @@
+mod balances;
+mod system;
+mod support;
+mod proof_of_existence;
+
+use crate::support::Dispatch;
+
+mod types {
+	pub type AccountId = String;
+	pub type Balance = u128;
+	pub type BlockNumber = u32;
+	pub type Nonce = u32;
+	pub type Extrinsic = crate::support::Extrinsic<AccountId, crate::RuntimeCall>;
+	pub type Header = crate::support::Header<BlockNumber>;
+	pub type Block = crate::support::Block<Header, Extrinsic>;
+}
+
+pub enum RuntimeCall {
+    Balances(balances::Call<Runtime>),
+}
+
+
+#[derive(Debug)]
+pub struct Runtime {
+	system: system::Pallet<Self>,
+	balances: balances::Pallet<Self>,
+}
+
+impl system::Config for Runtime {
+	type AccountId = types::AccountId;
+	type BlockNumber = types::BlockNumber;
+	type Nonce = types::Nonce;
+}
+
+impl balances::Config for Runtime {
+	type Balance = types::Balance;
+}
+
+impl Runtime {
+    fn new() -> Self {
+        Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
+    }
+
+    fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
+        self.system.inc_block_number();
+        if block.header.block_number != self.system.block_number() {
+            return Err("block number does not match what is expected")
+        }
+        for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
+            self.system.inc_nonce(caller.clone());
+            let _res = self.dispatch(caller, call).map_err(|e| {
+                eprintln!(
+                    "Extrinsic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
+                    block.header.block_number, i, e
+                )
+            });
+        }
+        Ok(())
+    }
+}
+
+impl crate::support::Dispatch for Runtime {
+    type Caller = <Runtime as system::Config>::AccountId;
+    type Call = RuntimeCall;
+    fn dispatch(
+		&mut self,
+        caller: Self::Caller,
+        runtime_call: Self::Call,
+    ) -> support::DispatchResult {
+		match runtime_call {
+			RuntimeCall::Balances(call) => {
+				self.balances.dispatch(caller, call)?;
+			}
+		}
+		Ok(())
+    }
+}
+
+fn main() {
+	let mut runtime = Runtime::new();
+	let alice = "alice".to_string();
+	let bob = "bob".to_string();
+	let charlie = "charlie".to_string();
+	runtime.balances.set_balance(&alice, 100);
+
+	let block_1 = types::Block {
+		header: support::Header { block_number: 1 },
+		extrinsics: vec![
+			support::Extrinsic {
+				caller: alice.clone(),
+				call: RuntimeCall::Balances(balances::Call::Transfer { to: bob, amount: 30 }),
+			},
+			support::Extrinsic {
+				caller: alice,
+				call: RuntimeCall::Balances(balances::Call::Transfer { to: charlie, amount: 30}),
+			}
+		],
+	};
+
+	runtime.execute_block(block_1).expect("invalid block");
+	assert_eq!(runtime.system.block_number(), 1);
+
+	println!("{:#?}", runtime);
+}
---
+++ src/system.rs
@@ -0,0 +1,62 @@
+use num::{One, Zero};
+use std::{collections::BTreeMap, ops::AddAssign};
+
+pub trait Config {
+	type AccountId: Ord + Clone;
+	type Nonce: Zero + One + Copy;
+	type BlockNumber: Zero + One + AddAssign + Copy;
+}
+
+#[derive(Debug)]
+pub struct Pallet<T: Config> {
+	block_number: T::BlockNumber,
+	nonce: BTreeMap<T::AccountId, T::Nonce>,
+}
+
+impl<T: Config> Pallet<T> {
+	pub fn new() -> Self {
+		Self { block_number: T::BlockNumber::zero(), nonce: BTreeMap::new() }
+	}
+
+	pub fn block_number(&self) -> T::BlockNumber {
+		self.block_number
+	}
+
+	pub fn inc_block_number(&mut self) {
+		self.block_number += T::BlockNumber::one();
+	}
+
+	pub fn inc_nonce(&mut self, who: T::AccountId) {
+		let nonce: T::Nonce = *self.nonce.get(&who).unwrap_or(&T::Nonce::zero());
+		let new_nonce: T::Nonce = nonce + T::Nonce::one();
+		self.nonce.insert(who, new_nonce);
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use crate::system::Config;
+
+	use super::Pallet;
+
+	#[test]
+	fn init_system() {
+		struct TestConfig;
+		impl Config for TestConfig {
+			type AccountId = String;
+			type BlockNumber = u32;
+			type Nonce = u32;
+		}
+		let alice = "Alice".to_string();
+		let bob = "Bob".to_string();
+		let mut system = Pallet::<TestConfig>::new();
+		assert_eq!(system.block_number(), 0);
+		system.inc_block_number();
+		assert_eq!(system.block_number(), 1);
+
+		assert_eq!(system.nonce.get(&alice), None);
+		system.inc_nonce(alice.clone());
+		assert_eq!(system.nonce.get(&alice), Some(&1));
+		assert_eq!(system.nonce.get(&bob), None);
+	}
+}
